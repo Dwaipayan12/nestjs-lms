2ced3c1c8cb7e7bc1763424b5832a53cf53b4b49
Dwaipayan Bhowmik <bhowmikdwaipayan505@gmail.com>
2026-01-19 22:49:19 +0530

    Add jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,13 @@ import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/registerUser.dto';
 import * as bcrypt from 'bcryptjs';
 import { LoginDto } from './dto/loginUser.dto';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
   async register(registerdto: RegisterDto) {
     console.log('registerUserDto', registerdto);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
@@ -16,7 +20,10 @@ export class AuthService {
       password: hashpassword,
     };
     const ans = this.userService.createUser(userdata);
-    return ans;
+    const payload = { sub: (await ans)._id };
+    const token = await this.jwtService.signAsync(payload);
+    console.log(token);
+    return token;
   }
   async login(logindto: LoginDto) {
     // const hashpassword = await bcrypt.hash(logindto.password, 10);
